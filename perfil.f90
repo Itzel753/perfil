d37636c6b99e4fd67e1a76a5eb89fcf47ea66c18
Itzel Julio <21itzel.julio@gmail.com>
2016-06-23 16:55:58 -0500

    Clean-up

--- perfil.f90
+++ perfil.f90
@@ -5,16 +5,14 @@ program perfil_par
 !*******************************************************************************
 
 implicit none
-real      :: boxx_1,boxy_1,boxx_2,boxy_2,delta_y,pi,angy
-integer   :: npart,i,bins,ii,biny
-double precision :: s2,s2sum
-double precision, dimension(:),allocatable :: pos_x,pos_y,ang_part,ex,ey,s2_array
+real(kind=8) :: boxx,boxy,delta_y,pi,angy
+integer      :: npart,i,bins,biny
+real(kind=8) :: s2,s2sum
+real(kind=8), dimension(:), allocatable :: pos_x,pos_y,ang_part,s2_array
 
 open (unit=8, file='confout.txt')
-
 read(8,*)
-read(8,*)boxx_2,boxy_2
-!write(*,*)boxx_2,boxy_2
+read(8,*)boxx,boxy
 read(8,*)
 read(8,*)npart
 read(8,*)
@@ -22,59 +20,44 @@ read(8,*)
 read(8,*)
 read(8,*)
 read(8,*)
-!write(*,*)boxx_2,boxy_2,npart
-
-bins=20
-pi=1.57
 
 allocate(pos_x(npart))
 allocate(pos_y(npart))
 allocate(ang_part(npart))
+
+do i = 1, npart
+   read(8,*) pos_x(i), pos_y(i), ang_part(i)
+   angy = pi - ang_part(i) 
+enddo
+
+close(8)
+
+bins=20
+pi=1.57
+
 allocate(s2_array(bins))
+!se da el tamaño del bin
+delta_y=boxy/dble(bins)
 
-!s2_array=0.d0
-do i=1,npart
-   read(8,*)pos_x(i),pos_y(i),ang_part(i)
-!write(1,*)pos_x(i),pos_y(i),ang_part(i)
-
-angy= pi - ang_part(i) 
+!se le asiga un bin a cada partícula 
+do i = 1, npart
+   biny = int(pos_y(i)/delta_y)+1
+   if (biny == 0) stop("biny es igual a cero")
+   !se calcula el parámetro de orden
+   s2 = cos(2*ang_part(i))
+   s2_array(biny) = s2_array(biny) + s2 !contabiliza cuantas particulas hay en una caja
 enddo
 
 open (unit=21, file='salida.dat')
-
-boxx_1=0.0
-boxy_1=0.0
-
-!se da el tamaño del bin
-delta_y=(boxy_2-boxy_1)/dble(bins)
-
-!se le asiga un bin a cada partícula 
-do i=1,npart
-biny=int((pos_y(i)+abs(boxy_1))/delta_y)+1
-if(biny.eq.0)stop("biny es igual a cero")
-
-!write(*,*)s2_array(biny)
-!se calcula el parámetro de orden
-s2=cos(2*ang_part(i))
-
-s2_array(biny) =s2_array(biny)+s2 !contabiliza cuantas particulas hay en una caja
+do i = 1, bins
+   write(21,*) i*delta_y, s2_array(i)
 enddo
-
-!do ii=1,bins
-!  write(21,*) s2_array(ii),ii*delta_y
-!enddo
-
-do ii=1,bins
-  write(21,*) ii*delta_y,s2_array(ii)
-enddo
+close(21)
 
 deallocate(pos_x)
 deallocate(pos_y)
 deallocate(ang_part)
 deallocate(s2_array)
 
-close(8)
-close(33)
-close(21)
 
 end program perfil_par
