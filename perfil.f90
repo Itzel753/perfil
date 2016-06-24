3a853dc4461bd4fa7f875f2f0d05d8b47dcdf709
Itzel Julio <21itzel.julio@gmail.com>
2016-06-23 20:13:09 -0500

    normalizar

--- perfil.f90
+++ perfil.f90
@@ -7,8 +7,8 @@ program perfil_par
 implicit none
 real(kind=8) :: boxx,boxy,delta_y,pi,angy
 integer      :: npart,i,bins,biny
-real(kind=8) :: s2,s2sum
-real(kind=8), dimension(:), allocatable :: pos_x,pos_y,ang_part,s2_array
+real(kind=8) :: s2
+real(kind=8), dimension(:), allocatable :: pos_x,pos_y,ang_part,s2_array,t_bin
 
 open (unit=8, file='confout.txt')
 read(8,*)
@@ -25,17 +25,21 @@ allocate(pos_x(npart))
 allocate(pos_y(npart))
 allocate(ang_part(npart))
 
+pi=3.141592653589793
 do i = 1, npart
    read(8,*) pos_x(i), pos_y(i), ang_part(i)
-   angy = pi - ang_part(i) 
+   ang_part(i) = pi/2.0 - ang_part(i) 
 enddo
 
 close(8)
 
 bins=20
-pi=1.57
-
+allocate(t_bin(bins))
 allocate(s2_array(bins))
+do i = 1, bins
+   s2_array(i) = 0.0
+enddo
+
 !se da el tamaño del bin
 delta_y=boxy/dble(bins)
 
@@ -46,11 +50,14 @@ do i = 1, npart
    !se calcula el parámetro de orden
    s2 = cos(2*ang_part(i))
    s2_array(biny) = s2_array(biny) + s2 !contabiliza cuantas particulas hay en una caja
+   t_bin(biny) = t_bin(biny) +1
 enddo
+
+s2_array (:) = s2_array(:)/t_bin(:)
 
 open (unit=21, file='salida.dat')
 do i = 1, bins
-   write(21,*) i*delta_y, s2_array(i)
+   write(21,*) i*delta_y, abs(s2_array(i))
 enddo
 close(21)
 
@@ -58,6 +65,6 @@ deallocate(pos_x)
 deallocate(pos_y)
 deallocate(ang_part)
 deallocate(s2_array)
-
+deallocate(t_bin)
 
 end program perfil_par
